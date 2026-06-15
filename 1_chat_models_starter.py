85f88e86127102efe4deca2672730e933118fb33
vidya27-mo <vidya@llumo.ai>
2026-06-15 18:07:16 +0530

    add chat model setup

---
+++ 1_chat_models_starter.py
@@ -0,0 +1,10 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-3.5")
+
+result = llm.invoke("Hi, How can you help me?")
+
+print(result)
